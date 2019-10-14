d70510ba124c342621d863597beb378a0903acd6
cu3l3bre <cu3l3bre@googlemail.com>
2019-10-14 15:21:16 +0200

    added some files

---
+++ mymodule.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# Define a function
+def world():
+    print("Hello, World!")
---
+++ test.py
@@ -0,0 +1,18 @@
+
+# importing my own module yeah
+import mymodule as mm
+
+#import tkinter as tk
+
+print("Hallo")
+a = 3
+b = 2
+
+   
+mm.world()
+
+
+
+#top = tk.Tk()
+#top.mainloop()
+
