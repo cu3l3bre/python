ebad4f70fa2a9287d06e7411272d3e4462994bdb
cu3l3bre <cu3l3bre@googlemail.com>
2019-10-15 13:54:22 +0200

    added folders

---
+++ 2019_10_14/test.py
@@ -0,0 +1,54 @@
+  # -*- coding: utf-8 -*-
+
+
+# importing my own module
+import mymodule as mm
+import numpy as np
+import random
+#import tkinter as tk
+
+print("Hallo")
+a = 3
+b = 2
+  
+mm.world()
+
+
+#print("HelloWorld")
+#print(np.sin(30))
+#b = np.sin(30)
+#c = np.sin(30)
+#print(b)
+
+
+a = 4
+if a == 3:
+    print("a ist 3")
+elif (a==4):
+    print("a ist 4") 
+else:
+    print("a ist iwas anderes")
+    print("Der ist mit drin")
+    print("Das hier auch noch")
+    
+print("Der sollte immer kommen")
+
+
+test2 = chr(177) # ist das +- zeichen
+print(test2)
+
+
+random.seed()
+
+
+for x in range(0, 10):
+    print(random.randint(1,1000))
+
+
+
+
+
+
+#top = tk.Tk()
+#top.mainloop()
+
---
+++ 2019_10_15/script.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 10:48:34 2019
+
+@author: Alfa
+"""
+
+mytuple = (1,2,3)
+
+mylist = list(mytuple)
+
+mylist[0] = 4
+
+mytuple = tuple(mylist)
+
+#mytuple[0] = 100
+
+
+
+myString = "HalloWelt"
+#myString[0]= "h" # ist so nicht erlaubt
+print(myString)
+print(myString[:1])
+print(myString[2:])
+myString = myString[:1] + "E" + myString[2:];
+
+print(myString)
+
+
+myString2 = "GutenTag"
+print(myString2)
+myList2 = list(myString2)
+myList2[6] = "A"
+myList2[7] = "G"
+myString2 = ''.join(myList2)
+print(myString2)
+
+
+x = 0
+while(x < 10):
+    print(x)
+    x += 1
+
+
+y = 10
+z = 20
+print(id(y),id(z))
+y = z
+print(id(y),id(z))
+
+#print(id(3))
+#print(id(1‬))
+print(id(2))
+print(id(3))
+#print(id(2))
+#print(id(1))
+
+a = 42949672967
+
+# key values
+u = {"a":1, "b":2, "c": "bla"}
+print(u["a"])
+print(u["b"])
+print(u["c"])
+print(type(u))
+
+
+v = {1:10,2:20,"3abc":30}
+print(v[1])
+print(v[2])
+print(v["3abc"])
+
+en_de = {"red":"rot","green":"gruen","blue":"blau","yellow":"gelb"}
+print(en_de["red"])
+#print(en_de.())
+
+dicttest = {"a":2, "a":3}
+print(dicttest["a"])
