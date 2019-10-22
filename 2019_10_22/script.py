bf67d6e8d1036b175a2cd29a1338c229674f4433
cu3l3bre <cu3l3bre@googlemail.com>
2019-10-22 15:26:19 +0200

    updated script

--- 2019_10_22/script.py
+++ 2019_10_22/script.py
@@ -1,27 +1,27 @@
-import MySQLdb
+import MySQLdb         #pip install mysqlclient
 import pandas as pd
 
 try:
     dbConnection = MySQLdb.connect(host="localhost",    # your host, usually localhost
                                    user="daniel",       # your username
                                    password ="gogogo",  # your password
-                                   db="mydb")           # name of the data base
+                                   db="sakila")          # name of the data base
     
-    #sqlString = "SELECT * FROM personen"
+    sqlString = "SELECT first_name, last_name FROM actor"
     #sqlString = "SELECT * FROM personen WHERE Vorname = 'Gunter'"
     
-    sqlString = """SELECT
-                    PANNr,
-                    Vorname
-                    ,Nachname,
-                    Telefon
-                    
-                    FROM `personen`
-                    
-                    JOIN pers_telefon
-                    ON personen.pers_telefon_ID = pers_telefon.ID
-                    
-                    WHERE PANNr = '4711' OR PANNr = '9999' """  
+#    sqlString = """SELECT
+#                    PANNr,
+#                    Vorname,
+#                    Nachname,
+#                    Telefon
+#                    
+#                    FROM `personen`
+#                    
+#                    JOIN pers_telefon
+#                    ON personen.pers_telefon_ID = pers_telefon.ID
+#                    
+#                    WHERE PANNr = '4711' OR PANNr = '9999' """  
     
     
     dataFrame = pd.read_sql(sqlString, dbConnection);
