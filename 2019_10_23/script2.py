beb9f404a4ef8b29ef69e9c5e71bb2134620d52d
cu3l3bre <cu3l3bre@googlemail.com>
2019-10-23 15:07:36 +0200

    changed script2.py

--- 2019_10_23/script2.py
+++ 2019_10_23/script2.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 import pandas as pd
 import sqlalchemy as sql
@@ -8,22 +7,19 @@ connect_string = 'mysql://daniel:gogogo@localhost/bike'
 sqlEngine = sql.create_engine(connect_string)
 dbConnection = sqlEngine.connect()
 
-sqlSelect = "select * from Reservierung"
+sqlSelect = "SELECT * FROM Reservierung"
 
 
 df = pd.read_sql_query(sqlSelect, sqlEngine)
 print(df, '\n')
 
 for index, row in df.iterrows():
-    if row['Posnr'] == 201:
+    if row['Posnr'] == 101:
         df['Anzahl'][index] += 1
 
-#print()
-#print(df)
 
 df.to_sql('mydatatable_temp', dbConnection, if_exists='replace', index=False);
 dbConnection.execute('REPLACE INTO Reservierung (SELECT * FROM mydatatable_temp);')
-
 
 df = pd.read_sql_query(sqlSelect, sqlEngine)
 print(df, '\n')
