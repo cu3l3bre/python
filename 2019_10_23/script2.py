628207aa027eb23e1257190b9512d674e4a00275
cu3l3bre <cu3l3bre@googlemail.com>
2019-10-24 09:01:34 +0200

    added comments

--- 2019_10_23/script2.py
+++ 2019_10_23/script2.py
@@ -9,18 +9,22 @@ dbConnection = sqlEngine.connect()
 
 sqlSelect = "SELECT * FROM Reservierung"
 
-
+# Get Data from DB
 df = pd.read_sql_query(sqlSelect, sqlEngine)
 print(df, '\n')
 
+# Look for PosNr 101 and add 1 to it
 for index, row in df.iterrows():
     if row['Posnr'] == 101:
         df['Anzahl'][index] += 1
 
-
+# Write dataframe to a new table for spamming
 df.to_sql('mydatatable_temp', dbConnection, if_exists='replace', index=False);
+
+# Replace the data in Rervierung with the data of the new table
 dbConnection.execute('REPLACE INTO Reservierung (SELECT * FROM mydatatable_temp);')
 
+# Get data again to see if it was successful
 df = pd.read_sql_query(sqlSelect, sqlEngine)
 print(df, '\n')
 
