7102f7e273a0c0764581015569eae592e0579fcd
cu3l3bre <cu3l3bre@googlemail.com>
2019-10-24 10:18:42 +0200

    changed script2.py

--- 2019_10_23/script2.py
+++ 2019_10_23/script2.py
@@ -2,9 +2,9 @@
 import pandas as pd
 import sqlalchemy as sql
 
-connect_string = 'mysql://daniel:gogogo@localhost/bike'
+connectionString = 'mysql://daniel:gogogo@localhost/bike'
 
-sqlEngine = sql.create_engine(connect_string)
+sqlEngine = sql.create_engine(connectionString)
 dbConnection = sqlEngine.connect()
 
 sqlSelect = "SELECT * FROM Reservierung"
@@ -13,7 +13,7 @@ sqlSelect = "SELECT * FROM Reservierung"
 df = pd.read_sql_query(sqlSelect, sqlEngine)
 print(df, '\n')
 
-# Look for PosNr 101 and add 1 to it
+# Look for PosNr 101 and add 1 to Anzahl
 for index, row in df.iterrows():
     if row['Posnr'] == 101:
         df['Anzahl'][index] += 1
@@ -21,7 +21,7 @@ for index, row in df.iterrows():
 # Write dataframe to a new table for spamming
 df.to_sql('mydatatable_temp', dbConnection, if_exists='replace', index=False);
 
-# Replace the data in Rervierung with the data of the new table
+# Replace the data in Reservierung with the data of the new table
 dbConnection.execute('REPLACE INTO Reservierung (SELECT * FROM mydatatable_temp);')
 
 # Get data again to see if it was successful
